d5a20abe5ba0bd800b5e1639516b61a043d10882
Romina Soto <romina.soto@xintec.cl>
2022-10-10 18:21:01 +0100

    fix file main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Nasa from "./components/NASA/nasa";
-import Tle from "./components/TLE/tle";
+import Nasa from "./components/NASA/Nasa";
+import Tle from "./components/TLE/Tle";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
